ebd032c155f17d19f329cbe3bd77a9b89eb7c860
Justina <justcho5@gmail.com>
2019-11-10 08:43:59 -0600

    tcav bugs

--- tcav/tcav.py
+++ tcav/tcav.py
@@ -13,7 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
+import sys
+sys.path.append("/mnt/gpucluster/packages/tcav")
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
